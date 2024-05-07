15728f65459696afc54c0b6e0f626b6d1ba4cc3a
TimSpektrov <timurbadalbaev19@gmail.com>
2024-05-07 12:55:10 +0300

    update images stages-redesign

--- src/components/screens/Home/Home.tsx
+++ src/components/screens/Home/Home.tsx
@@ -1,24 +1,13 @@
 import { FC } from 'react'
 import Head from 'next/head'
 import { Hero } from './Hero'
-import { WorksList } from '@/components/ui/WorksList'
 import { Design } from '@/components/screens/Home/Design'
 import { Services } from './Services'
-
-import style from './Home.module.scss'
 import { NewFooterFeedback } from '@/components/ui/NewFooterFeedback';
 import { Telegram } from './Telegram'
 import { NewWorksList } from '@/components/ui/NewWorksList';
 
 export const Home: FC<any> = ({content}) => {
-
-    const data =  {
-      telegram: {
-        description: 'Узнайте, как использование дизайна может значительно улучшить ваши бизнес-показатели, получите доступ к&nbsp;ценным советам и&nbsp;стратегиям.',
-        textButton: 'Подписаться'
-      }
-    }
-
   return (
     <>
       <Head>
@@ -33,7 +22,7 @@ export const Home: FC<any> = ({content}) => {
       />
       <Design title={content.design.title} description={content.design.description} />
       <Services services={content.services} />
-      <Telegram title={content.telegram.title} description={data.telegram.description} />
+      <Telegram title={content.telegram.title} description={content.telegram.description} />
       <NewFooterFeedback />
     </>
   );
